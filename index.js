fd70d786cc69c5a7231d852f73c806e74386c4b4
otanikotani <otanikotani@gmail.com>
2021-06-28 11:15:53 -0400

    feat(release): Semantic release

--- index.js
+++ index.js
@@ -20,20 +20,27 @@ function execShellCommand(cmd) {
     const gitmeta = core.getInput('gitmeta');
     const meta = core.getInput('meta');
     const metrics = core.getInput('metrics');
+    const templateOutput = core.getInput('templateOutput');
+    const template = core.getInput('template');
+    const s3Path = `${bucket}/${serviceName}/${version}`;
 
-    console.log(`\n\tService name: ${serviceName}\n\tBucket: ${bucket}\n\tVersion: ${version}\n\tGitmeta: ${gitmeta}\n\tMeta: ${meta}\n\tMetrics: ${metrics}`)
+    console.log(`\n\tService name: ${serviceName}\n\tBucket: ${bucket}\n\tVersion: ${version}\n\tGitmeta: ${gitmeta}\n\tMeta: ${meta}\n\tMetrics: ${metrics}\n\tTemplateOutput: ${templateOutput}`)
 
     if (gitmeta) {
-      console.log(await execShellCommand(`aws s3 cp ${gitmeta} s3://${bucket}/${serviceName}/${version}/gitMeta`));
+      console.log(await execShellCommand(`aws s3 cp ${gitmeta} s3://${s3Path}/gitMeta`));
     }
+
     if (meta) {
-      console.log(await execShellCommand(`aws s3 cp ${meta} s3://${bucket}/${serviceName}/${version}/meta.yml`));
+      console.log(await execShellCommand(`aws s3 cp ${meta} s3://${s3Path}/meta.yml`));
     }
 
     if (metrics) {
-      console.log(await execShellCommand(`aws s3 cp ${metrics} s3://${bucket}/${serviceName}/${version}/metrics.yml`));
+      console.log(await execShellCommand(`aws s3 cp ${metrics} s3://${s3Path}/metrics.yml`));
     }
 
+    if (templateOutput && template) {
+      console.log(await execShellCommand(`aws s3 cp ${template} s3://${s3Path}/${templateOutput}`));
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
